438498f79047875fe58ef3a2f115e843ae244820
Helena Maria Alfonso Vera <alfonsoveramaria@gmail.com>
2026-04-23 16:00:49 -0500

    Cambios23042026

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,11 @@
-import { Routes } from '@angular/router'; // v1.0.1
-import { Dashboard } from './dashboard/dashboard';
-import { VehiculosComponent } from './vehiculo/vehiculos';
-import { Aprendices } from './users/aprendices';
+import { Routes } from '@angular/router';
+import { VehiculosComponent } from './features/vehiculo/vehiculos';
+import { Aprendices } from './features/users/aprendices';
 import { authGuard } from './core/guards/auth-guard';
-import { Fichas } from './ficha/ficha';
-import { Inicio } from './inicio/inicio';
-import { Users } from './users/users';
-import { RolesComponent } from './roles/roles';
+import { Fichas } from './features/ficha/ficha';
+import { Inicio } from './features/inicio/inicio';
+import { Users } from './features/users/users';
+import { RolesComponent } from './features/roles/roles';
 import { AdminLayoutComponent } from './core/components/admin-layout/admin-layout';
 
 export const routes: Routes = [
@@ -15,8 +14,6 @@ export const routes: Routes = [
     loadChildren: () => import('./auth/auth.routes').then(m => m.AUTH_ROUTES)
   },
   {
-    // path: 'dashboard',
-    // component: Dashboard,
     path: '',
     component: AdminLayoutComponent,
     canActivate: [authGuard],
